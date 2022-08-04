08a60416be921c42bd2efb3515b7fae96bf4f6b5
calfreeman <ctfreeman1s@gmail.com>
2022-08-04 00:39:56 -0400

    reads json file

--- connect.py
+++ connect.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python
 import psycopg2
+import json
+import sys
 from config import config
+from psycopg2 import connect, Error
+
+def test():
+    print('test')
 
 def connect():
+
+    # accept command line arguments for the Postgres table name
+    if len(sys.argv) > 1:
+        table_name = '_'.join(sys.argv[1:])
+    else:
+        # ..otherwise revert to a default table name
+        table_name = "json_data"
+
+    print ("\ntable name for JSON data:", table_name)
+
+    # use Python's open() function to load the JSON data
+    with open("./bin/final.json") as json_data:
+
+        # use load() rather than loads() for JSON files
+        record_list = json.load(json_data)
+
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
@@ -19,7 +41,7 @@ def connect():
 	# execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
-
+        test()
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
