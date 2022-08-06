d582625b3bedc174ceb25cbd2eae169c20d2295e
calfreeman <ctfreeman1s@gmail.com>
2022-08-06 15:30:52 -0400

    progress

--- connect.py
+++ connect.py
@@ -5,57 +5,11 @@ import sys
 from config import config
 from psycopg2 import connect, Error
 
-def test():
-    # brand_col = []
-    # gram_col = []
-    # image_col = []
-    # name_col = []
-    # price_col = []
-    # qty_col = []
-    # status_col = []
-    # strain_col = []
-    # type_col = []
-
-    print('test')
-    # accept command line arguments for the Postgres table name
-    if len(sys.argv) > 1:
-        table_name = '_'.join(sys.argv[1:])
-    else:
-        # ..otherwise revert to a default table name
-        table_name = "json_data"
-
-    print ("\ntable name for JSON data:", table_name)
-
-    # use Python's open() function to load the JSON data
+def dataFetch():
+    # use Python's open() function to load the JSON data and return
     with open("./bin/final.json", "r") as json_file:
-
         record_list = json.load(json_file)
-        print ("\nJSON records object type:", type(record_list)) # should return "<class 'list'>"
-        item_len = len(record_list)
-        #print(item_len)
-        for num in range(0,item_len):
-            print(record_list[num])
-            print(num)
-        
-        # brand_col.append(record_list[num]["brand_col"])
-        # gram_col.append()
-        # image_col.append()
-        # name_col.append()
-        # price_col.append()
-        # qty_col.append()
-        # status_col.append()
-        # strain_col.append()
-        # type_col.append()
-
-        # for item in record_list("json_data"):
-        #     print(record_list[int(item)])
-        # # concatenate the SQL string
-        # table_name = "json_data"
-        # sql_string = "INSERT INTO %s (%s)\nVALUES %s" % (
-        #     table_name,
-        #     ', '.join(columns),
-        #     values_str
-        # )
+    return record_list
 
 def connect():
     try:
@@ -70,22 +24,23 @@ def connect():
         cur = conn.cursor()
         print ("\ncreated cursor object:", cur)
 
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
         #test json loader and parser
-        test()
-        #test post
-        postgres_insert_query = """ INSERT INTO json_data (id, brand_col, gram_col, image_col, name_col, price_col, qty_col, status_col, strain_col, type_col) VALUES (%s,%s,%s, %s, %s, %s, %s, %s, %s, %s)"""
-        record_to_insert = ("5231b533-ba17-4787-98a3-f2df37de2ad7", "brandTest", 1, "testImg", "testName", 11, 12, "testStatus", "testStrain", "testType")
-        cur.execute(postgres_insert_query, record_to_insert)
-        conn.commit()
-        count = cur.rowcount
-        print(count, "Record inserted successfully")
+        dataFetched = dataFetch()
+        #print(dataFetched)
+        item_len = len(dataFetched)
+        for num in range(0,item_len):
+            print(dataFetched[num])
+
+        # postgres_insert_query = """ INSERT INTO json_data (id, brand_col, gram_col, image_col, name_col, price_col, qty_col, status_col, strain_col, type_col) VALUES (%s,%s,%s, %s, %s, %s, %s, %s, %s, %s)"""
+        # record_to_insert = ("5231b533-ba17-4787-98a3-f2df37de2ad7", "brandTest", 1, "testImg", "testName", 11, 12, "testStatus", "testStrain", "testType")
+        # cur.execute(postgres_insert_query, record_to_insert)
+        # conn.commit()
+        # count = cur.rowcount
+        # print(count, "Record inserted successfully")
 
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
-        print(db_version)
+        #print(db_version)
 
     # close the communication with the PostgreSQL
         cur.close()
@@ -95,7 +50,6 @@ def connect():
         if conn is not None:
             conn.close()
             print('Database connection closed.')
-
 
 if __name__ == '__main__':
     connect()
