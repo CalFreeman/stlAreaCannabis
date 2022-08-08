fb534e96b58cdd243d660a3adfb6e628ae080cfe
calfreeman <ctfreeman1s@gmail.com>
2022-08-07 23:51:09 -0400

    dynamic URL scraping based on dispensary UUID

--- bin/dutchie_parser.sh
+++ bin/dutchie_parser.sh
@@ -12,7 +12,7 @@ echo "depo_id: $DISP_ID"
 
 SCRAP_URL=( $(psql -d testdb -U myuser -t -h 0.0.0.0 -p 5432 -c "SELECT url FROM dispensaries WHERE id = '28a2786e-7741-40ca-98d2-07ea464f4950';"))
 echo "SCRAP_URL: $SCRAP_URL"
-curl 'https://stcharles.heyawellness.com/graphql?operationName=FilteredProducts&variables=%7B%22includeEnterpriseSpecials%22%3Afalse%2C%22includeCannabinoids%22%3Atrue%2C%22productsFilter%22%3A%7B%22dispensaryId%22%3A%225f5fae36c702e000fb457605%22%2C%22pricingType%22%3A%22med%22%2C%22strainTypes%22%3A%5B%5D%2C%22subcategories%22%3A%5B%5D%2C%22Status%22%3A%22Active%22%2C%22types%22%3A%5B%22Flower%22%5D%2C%22useCache%22%3Afalse%2C%22sortDirection%22%3A1%2C%22sortBy%22%3Anull%2C%22isDefaultSort%22%3Atrue%2C%22bypassOnlineThresholds%22%3Afalse%2C%22isKioskMenu%22%3Afalse%2C%22removeProductsBelowOptionThresholds%22%3Atrue%7D%2C%22page%22%3A0%2C%22perPage%22%3A50%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22aca5bbf9093bf1ccd28d3d9f010de5cd18e50dcbe96e4ddcda1e530fc0eabe2b%22%7D%7D' >results.json
+curl $SCRAP_URL >results.json
 cat results.json |jq .data[].products[].Status >status
 cat results.json |jq .data[].products[].Prices[] >prices
 cat results.json |jq .data[].products[].strainType >strains
