ad7a23bcf84f35d7b959a7745003f65146e66d6a
calfreeman <ctfreeman1s@gmail.com>
2022-08-08 14:15:34 -0400

    moving away from shell script to fetch json_data, working publisher fetch

--- showMeScraper/publisher.py
+++ showMeScraper/publisher.py
@@ -2,9 +2,19 @@
 import psycopg2
 import json
 import sys
-from .config import config
-from psycopg2 import connect, Error
+import urllib
 import uuid
+import subprocess
+from config import config
+from psycopg2 import connect, Error
+
+# fetch url to scrap
+def fetchUrlpsqlQuery(columnName, table, dispensary_id):
+    psql_select_Query = "select " + columnName + " from " + table + " where id = '" + dispensary_id + "';"
+    return psql_select_Query
+
+def publish():
+    subprocess.run(["../bin/data_parser.sh", "947808ec-f091-4507-bdc5-a1fb065f689d"])
 
 def dataFetch():
     # use Python's open() function to load the JSON data and return
@@ -13,6 +23,9 @@ def dataFetch():
     return record_list
 
 def connect():
+    columnName = "flower_url"
+    table = "dispensaries"
+    dispensary_id = "947808ec-f091-4507-bdc5-a1fb065f689d" 
     try:
         # read connection parameters
         params = config()
@@ -20,33 +33,41 @@ def connect():
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
-        
+
         # create a cursor
         cur = conn.cursor()
         print ("\ncreated cursor object:", cur)
 
-        # test json loader and parser
-        dataFetched = dataFetch()
-        item_len = len(dataFetched)
+        #need to provid param values
+        url = fetchUrlpsqlQuery(columnName, table, dispensary_id)
+        print (url)
+        cur.execute(url)
+        json_blob = cur.fetchone()
+        print(json_blob)
+        #publish()
 
-        #fetch dispensary_id
-        dispensary_id = 
-        for num in range(0,item_len):
-            # generate UUID
-            myuuid = uuid.uuid4()
-            myuuidStr = str(myuuid)
-            # insert data
-            postgres_insert_query = """ INSERT INTO json_data (id, companies, brand, gram, image, name, price, quantity, status, strain, type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-            record_to_insert = (myuuidStr, dispensary_id, dataFetched[num]["brand"], dataFetched[num]["gram"], dataFetched[num]["image"], dataFetched[num]["name"], dataFetched[num]["price"], dataFetched[num]["quantity"], dataFetched[num]["status"], dataFetched[num]["strain"], dataFetched[num]["type"])
-            cur.execute(postgres_insert_query, record_to_insert)
-            conn.commit()
-            count = cur.rowcount
-            print(count, "Record inserted successfully")
+        # # test json loader and parser
+        # dataFetched = dataFetch()
+        # item_len = len(dataFetched)
 
-        # display the PostgreSQL database server version
-        cur.execute('SELECT version()')
-        db_version = cur.fetchone()
-        print(db_version)
+        # #fetch dispensary_id
+        # dispensary_id = '947808ec-f091-4507-bdc5-a1fb065f689d'
+        # for num in range(0,item_len):
+        #     # generate UUID
+        #     myuuid = uuid.uuid4()
+        #     myuuidStr = str(myuuid)
+        #     # insert data
+        #     postgres_insert_query = """ INSERT INTO json_data (id, companies, brand, gram, image, name, price, quantity, status, strain, type) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        #     record_to_insert = (myuuidStr, dispensary_id, dataFetched[num]["brand"], dataFetched[num]["gram"], dataFetched[num]["image"], dataFetched[num]["name"], dataFetched[num]["price"], dataFetched[num]["quantity"], dataFetched[num]["status"], dataFetched[num]["strain"], dataFetched[num]["type"])
+        #     cur.execute(postgres_insert_query, record_to_insert)
+        #     conn.commit()
+        #     count = cur.rowcount
+        #     print(count, "Record inserted successfully")
+
+        # # display the PostgreSQL database server version
+        # cur.execute('SELECT version()')
+        # db_version = cur.fetchone()
+        # print(db_version)
 
     # close the communication with the PostgreSQL
         cur.close()
